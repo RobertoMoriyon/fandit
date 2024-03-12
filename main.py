94fc3f2f9ddb7575b0755853f38c08226d49a796
Roberto Moriyón <moriyon@gmail.com>
2024-03-12 11:05:58 +0100

    Form update

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('Fandit Subvenciones')
 
 st.write('Bienvenido a Fandit Subvenciones, una aplicación que te ayudará a encontrar subvenciones para tu proyecto.')
+
+
+with st.form(key='form'):
+    fecha_elegida = st.date_input("Seleccione la fecha para buscar:")
+    boton_submit = st.form_submit_button("Consultar")
